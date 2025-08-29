abb849a6d936bb7a5b7a2df5955fb5ad59497e7e
Ironboy <ironboy@users.noreply.github.com>
2025-08-29 10:41:16 +0200

    ok

---
+++ frontend/start-page.js
@@ -0,0 +1,8 @@
+export function startPageContent() {
+  return `
+    <h1>Start</h1>
+    <p>Välkommen till våra sökmotor för metadata, där det är tänkt vi så småningom ska kunna söka i viktiga filer från företagets filservrar.</p>
+    <p>Vi har börjat med musikfiler och nuvarande import gjorde 2025-08-27.</p>
+    <p>Kontakta din vänliga <b>data manager</b> Thomas om du känner till fler musikfiler som ska indexeras. <a href="mailto:thomas@nodehill.com">thomas@nodehill.com</a>.</p>
+  `;
+}
